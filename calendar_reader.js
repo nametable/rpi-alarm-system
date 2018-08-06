0491146a63d75c3328ebdf01a42b4df527119000
Logan Bateman <lbateman0199@gmail.com>
2018-08-06 05:01:17 -0400

    Add calendar event functionality

--- calendar_reader.js
+++ calendar_reader.js
@@ -1,3 +1,34 @@
+/*
+//  2018 Logan Bateman
+//
+//
+*/
 const {google} = require('googleapis');
+var calendar = google.calendar('v3');
 
-var sheets = google.calendar('v3');
+module.exports= class calendar_reader{
+  static getEventsDuringDay(jwtClient, calendarId, date){
+    return new Promise(function(resolve, reject){
+      //(yesterday=new Date()).setDate((new Date()).getDate()-1);
+      var resp;
+      var minDate=new Date(date);
+      var maxDate=new Date(date);
+      minDate.setHours(0); minDate.setMinutes(0); minDate.setSeconds(0);
+      maxDate.setHours(23); maxDate.setHours(59); maxDate.setSeconds(59);
+      calendar.events.list({
+         auth: jwtClient,
+         calendarId: calendarId,
+         maxResults:50,
+         orderBy: "updated",
+         timeMin: minDate.toISOString(),
+         timeMax: maxDate.toISOString()
+      }, function (err, response) {
+         if (err) {
+             console.log('The API returned an error: ' + err);
+             reject(err);
+         }
+         if(response)resolve(response.data.items); //this would be the list of events
+      });
+    });
+  }
+}
