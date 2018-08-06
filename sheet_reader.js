1ab649a7cab8ff8f42e820ee8d095b5b550a77b3
Logan Bateman <lbateman0199@gmail.com>
2018-08-05 23:51:23 -0400

    Added event list reader - in progress

--- sheet_reader.js
+++ sheet_reader.js
@@ -1,5 +1,12 @@
-const {google} = require('googleapis');
+/*
+//  2018 Logan Bateman
+//  sheet_reader: a class which contains functions for reading a Google Spreadsheet
+//
+//
+*/
 
+const {google} = require('googleapis');
+const constants = require('./constants.js');
 var sheets = google.sheets('v4');
 module.exports = class sheet_reader{
   static readSpreadsheet(jwtClient, sheetId)
@@ -11,11 +18,13 @@ module.exports = class sheet_reader{
       },
       function(err, response)
       {
-        resolve(response);
+        if(response){resolve(response)}
+        else if (err) {reject(err)}
       });
     });
     return resp;
   }
+  //Return actual values from a spreadsheet at the specified range
   static readSheet(jwtClient, spreadsheetId, range){
     return new Promise(function(resolve, reject){
       sheets.spreadsheets.values.get({
@@ -26,6 +35,38 @@ module.exports = class sheet_reader{
         if(response){resolve(response)}
         else if (err) {reject(err)}
       })
+    });
+  }
+  //Return a list of events from a Schedule spreadsheet based on sheetid and date
+  static getEventList(jwtClient, spreadsheetId, date){
+    return new Promise(function(resolve, reject){
+      console.log(constants.dow_list);
+      var weekday=constants.dow_list[date.getDay()];
+      console.log("Today is " + weekday);
+
+      var readSheetData=sheet_reader.readSheet(jwtClient, spreadsheetId, weekday);
+      readSheetData.then(function(response){
+        var eventList;
+        var colTime, colEvent, colDescription;
+        for(var columnNumber; columnNumber<response.data.values[0].length; columnNumber++){
+          switch(response.data.values[0][columnNumber]){
+            case "Time":
+              colTime=columnNumber;
+              break;
+            case "Event":
+              colEvent=columnNumber;
+              break;
+            case "Description":
+              colDescription=columnNumber;
+              break;
+            default:
+          }
+        };
+        for(var rowNumber=1; rowNumber<response.data.values.length; rowNumber++){
+
+        };
+        resolve(eventList);
+      });
     });
   }
 }
