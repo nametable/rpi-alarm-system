23075fc2121eb44abf0539d9d5596a767fb15ffd
Logan Bateman <lbateman0199@gmail.com>
2018-08-07 23:23:01 -0400

    Fixed typos and problems

--- code_parser.js
+++ code_parser.js
@@ -1,36 +1,39 @@
 
-module.exports= code_parser {
+module.exports= class code_parser {
   static getId(identifier, control_settings){
+    if((!identifier)|(!control_settings)){return null}
     if(identifier.indexOf("#")!=-1){
       return identifier.split("#")[1]
     }else if (identifier.indexOf("$")!=-1){
       var varname=identifier.split("$")[1]
-      scheduleId=control_settings.schedules[varname];
-      calendarId=control_settings.calendars[varname];
+      var scheduleId=control_settings.schedules[varname];
+      var calendarId=control_settings.calendars[varname];
       if(scheduleId){return scheduleId};
       if(calendarId){return calendarId};
     }else{return undefined;}
   }
   static getScheduleFromCalendarEvents(calEventList, control_settings){
-    var calEventsSplit[];
+    var calEventsSplit=[];
     var highestVal=0;
     var highestIndex=0;
-    for(var calEventCounter=0;calEventCounter< calEventCounter.length; calEventCounter++){
-      if(calEventList[calEventCounter].description.indexOf("#")!=-1){
-        calEventsSplit[calEventCounter]=calEventList[calEventCounter].description.split("#");
+    for(var calEventCounter=0;calEventCounter< calEventList.length; calEventCounter++){
+      if(calEventList[calEventCounter].summary.indexOf("#")!=-1){
+        calEventsSplit[calEventCounter]=calEventList[calEventCounter].summary.split("#");
         if (parseInt(calEventsSplit[calEventCounter][0])>=highestVal){
-          highestVal=parseInt(calEventsSplit[calEventCounter][0];
+          highestVal=parseInt(calEventsSplit[calEventCounter][0]);
           highestIndex=calEventCounter;
         }
       }
-      if(calEventList[calEventCounter].description.indexOf("$")!=-1){
-        calEventsSplit[calEventCounter]=calEventList[calEventCounter].description.split("$");
+      if(calEventList[calEventCounter].summary.indexOf("$")!=-1){
+        calEventsSplit[calEventCounter]=calEventList[calEventCounter].summary.split("$");
         calEventsSplit[calEventCounter][1]=control_settings.calendars[calEventsSplit[calEventCounter][1]];
         if (parseInt(calEventsSplit[calEventCounter][0])>=highestVal){
-          highestVal=parseInt(calEventsSplit[calEventCounter][0];
+          highestVal=parseInt(calEventsSplit[calEventCounter][0]);
           highestIndex=calEventCounter;
         }
       }
     }
+    if(calEventsSplit=[]){console.error("Bad calendar event list... no schedule found.");return null}else{if (!calEventsSplit[highestIndex][1]){return null;}}
+    return this.getId(calEventsSplit[highestIndex][1],control_settings)
   }
 }
