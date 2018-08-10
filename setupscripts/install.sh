332d9cc3f594c883d4ed3c34be807f38cebb843d
Logan Bateman <lbateman0199@gmail.com>
2018-08-10 03:38:50 -0400

    Move pm2 startup  script info

--- setupscripts/install.sh
+++ setupscripts/install.sh
@@ -17,9 +17,9 @@ cd rpi-alarm-system
 npm install .
 sudo npm install pm2@latest -g
 pm2 start index.js --name rpi-alarm-system
-`pm2 startup`
 pm2 save
 pm2 stop 0
 echo "You will need to place your download your auth key as 'serviceaccountkey.js'"
 echo "You will also need to configure 'config.json'. Use config.json.sample to help"
-echo "More detailed instructions are at "
+echo "More detailed instructions are at https://github.com/nametable/rpi-alarm-system/blob/master/docs/setup.md"
+pm2 startup
